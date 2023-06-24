a9aee7fc1d3045aa78e869dad258ff4ad28d3ab7
nataraj16 <137592509+nataraj16@users.noreply.github.com>
2023-06-24 10:34:24 +0530

    Add files via upload

---
+++ 19-6-23/push.py
@@ -0,0 +1,4 @@
+def push():
+    element=("enter the element into stack")
+    stack.append(element)
+    print(stack)
---
+++ 19-6-23/pushpop.py
@@ -0,0 +1,5 @@
+stack=[2,3,4,5]
+def push():
+    element=input("enter the element to the stack")
+    stack.append(element)
+    print(stack)
